bc138ecd2a6fabd2df0f2063a617192d7022d464
sunyuting <sleepsun123@163.com>
2020-03-11 12:39:06 +0800

    chenged react build file

---
+++ static/precache-manifest.40f65d62acf738c972fd49464399a240.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "7e8b64c57c90ab08f7b8",
+    "url": "/static/css/main.e26fcfd1.chunk.css"
+  },
+  {
+    "revision": "7e8b64c57c90ab08f7b8",
+    "url": "/static/js/main.3d30f94d.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "4929985ae070a1cd2f0d",
+    "url": "/static/css/2.38230622.chunk.css"
+  },
+  {
+    "revision": "4929985ae070a1cd2f0d",
+    "url": "/static/js/2.210b5df8.chunk.js"
+  },
+  {
+    "revision": "bc53d9ca809f4685f76397c11af21c85",
+    "url": "/index.html"
+  }
+];
